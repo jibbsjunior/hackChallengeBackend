ed64de15f63b9169154f471350394b07ce73bc72
Adebayo Rilwam Ajibola <ajibolarilwan14@gmail.com>
2021-01-05 16:49:03 +0100

    modify server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require('path');
 
 const route = require("./routes");
 
@@ -16,6 +17,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/api", route);
+
+// serve static assets in production
+if (process.env.NODE_ENV == 'production') {
+	// serve static folder
+	app.use(express.static('/build'));
+
+	app.get('*',(req,res)=>res.sendFile(path.resolve()));
+}
 
 var port = (process.env.PORT || '5000');
 app.set('port', port);
